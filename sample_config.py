98eb56645030a667319fc1f7ac483ea04760b31d
winndy8 <125177960+winndy8@users.noreply.github.com>
2023-02-17 14:15:24 +0530

    Update sample_config.py

--- sample_config.py
+++ sample_config.py
@@ -12,19 +12,19 @@ logging.getLogger("pyrogram").setLevel(logging.WARNING)
 class Config(object):
 
     # Get a bot token from botfather
-    TG_BOT_TOKEN = os.environ.get("TG_BOT_TOKEN", "")
+    TG_BOT_TOKEN = os.environ.get("TG_BOT_TOKEN", "6153177382:AAFHSghOCWr-wEQOBpE_1T7Bu91-sAthPGY")
 
     # Get from my.telegram.org
-    APP_ID = int(os.environ.get("APP_ID", ""))
+    APP_ID = int(os.environ.get("APP_ID", "2790569"))
 
     # Get from my.telegram.org
-    API_HASH = os.environ.get("API_HASH", "")
+    API_HASH = os.environ.get("API_HASH", "7c72307ff7c7e44d2b416e551145f5ba")
 
     # Generate a user session string
-    TG_USER_SESSION = os.environ.get("TG_USER_SESSION", "")
+    TG_USER_SESSION = os.environ.get("TG_USER_SESSION", "BQB7iIRYmGsUgoQdl6EuYCFkIPnAp1K7Bch9LrRC6T-IyuvU-5RIfOwxbitxtAzvZIf6H2fgnDPKFJp3nhUYti2F3S9ZEK7KyLMlfvSVCvEP81on8end0Z_xDJXKbFVncpypuWLrr8vg9hBSVfdccAfpl9Yf3YYnxG_S21w1Kx1E1iDa8nexpxVil2hX4Nria4vmNd3gVDau85MIzYKfN3nEjfhk9NMlgncMhJyZNmWoNA0huV6UDrYGHrkO8h3aOHkxbCfg5TkN3rzt7S6HIe4jp9ZVKAd2evSP1mduINtPKgHScvO5rpE7axbPRgLheQdn7Hg9MlRSgfHRgLS26dxxAAAAATy6BAcA")
 
     # Database URI
-    DB_URI = os.environ.get("DATABASE_URL", "")
+    DB_URI = os.environ.get("DATABASE_URL", "mongodb+srv://clone:clone@cluster0.go11yez.mongodb.net/?retryWrites=true&w=majority")
 
 
 def LOGGER(name: str) -> logging.Logger:
